5e08975a2b7419485d7f236062b2be43fff8ccfb
ashikurrahman-1 <webdeveloperashik101@gmail.com>
2021-09-11 14:25:36 +0600

    about section

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
 new WOW().init();
+
+// owl carousel
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items: 1,
+        loop: true,
+        nav: true,
+        autoplay: true,
+        autoplayTimeout: 2500,
+        smartSpeed: 1000,
+        autoplayHoverPause: true,
+    });
+  });
+
