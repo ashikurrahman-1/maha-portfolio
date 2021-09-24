79df7d3fb181d57edd47f7028c27e657ca8fce7c
ashikurrahman-1 <webdeveloperashik101@gmail.com>
2021-09-24 09:25:08 +0600

    some js features

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
 new WOW().init();
 
+
+// navbar hide when scroll down
+var lastScrollTop;
+navbar = document.getElementById('navbar');
+window.addEventListener('scroll',function(){
+  var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+  if(scrollTop > lastScrollTop){
+    navbar.style.top='-90px';
+  }
+  else{
+    navbar.style.top='0';
+  }
+  lastScrollTop = scrollTop;
+});
+
 // About owl carousel
 $(document).ready(function(){
     $(".projects").owlCarousel({
@@ -44,3 +60,30 @@ $(document).ready(function(){
           },
     });
 });
+
+
+// Back to top button
+var mybutton = document.getElementById("myBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction(){
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+
+    document.getElementById("myBtn").style.transition = "all 2s";
+  }
+  else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrolltop = 0;
+  document.documentElement.scrollTop = 0;
+
+  var body = $("html, body");
+      body.stop().animate({scrollTop:0}, 500, 'swing', function() { 
+           console.log("Animation has finished");
+      });
+}
